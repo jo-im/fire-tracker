59afb8a0f293fdf02d5eea50402bd4fe3ffb9b1d
Ben Titcomb <btitcomb@scpr.org>
2017-12-05 19:47:46 -0800

    Preload search backgrounds.  Worked the first time I wrote it!  Yes.

--- app/components/fire-search.js
+++ app/components/fire-search.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import fetch from 'fetch';
 
 export default Ember.Component.extend({
   classNames: ['fire-search'],
   fastboot: Ember.inject.service(),
-  style: Ember.computed('background', function(){
+  style: Ember.computed('background', 'willRender', function(){
     let bg = this.get('background');
     if(bg){
       return `background-image:  url('${bg}'), linear-gradient(to bottom, #0d0101 0%, #a24e13 100%);`;
@@ -16,6 +17,9 @@ export default Ember.Component.extend({
     let i = Math.floor(Math.random() * 20) + 1;
     let n = Array(Math.max(2 - String(i).length + 1, 0)).join(0) + i;
     return `/images/backgrounds/Search_Background-Image${n}.jpg`;
+  }),
+  willRender: Ember.computed('background', function(){
+    return fetch(this.get('background'));
   }),
   attributeBindings: ['style'],
   actions: {
