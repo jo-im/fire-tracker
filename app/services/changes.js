14413430b83ffff668491d899595115cadbe1c76
Ben Titcomb <btitcomb@scpr.org>
2018-01-18 15:07:52 -0800

    Prevent longpolling in Fastboot.

--- app/services/changes.js
+++ app/services/changes.js
@@ -5,11 +5,13 @@ import ENV from '../config/environment';
 import fetch from 'fetch';
 
 export default Ember.Service.extend(Ember.Evented, {
+  fastboot: Ember.inject.service(),
   init(){
     this._super(...arguments);
     this._listen();
   },
   _listen(){
+    if(this.get('fastboot.isFastBoot') || this.get('isDestroying') || this.get('isDestroyed')) return;
     fetch(`${ENV.couchdb.endpoint}/fires/_changes?since=now&feed=longpoll`)
       .then(resp => resp.json())
       .then(json => {
@@ -21,3 +23,4 @@ export default Ember.Service.extend(Ember.Evented, {
       .then(() => this._listen());
   }
 });
+
